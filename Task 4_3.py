3b4c1d3d3200a8a3552f2b986f047177c9c982e9
Evgeny_Lemeshevsky <elemeshevsky@gmail.com>
2022-02-22 20:49:12 +0300

    Add changes for 4 task

--- Task 4_3.py
+++ Task 4_3.py
@@ -19,26 +19,21 @@ def create_and_add_sentence(text):
     text += ' ' + last_sentence
     return text
 
-
-separators = r'[.|:]\s*'
-text = '''homEwork:
-	tHis iz your homeWork, copy these Text to variable.
-
-	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
-
-	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
-
-	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
-
-normalized_text = normalize_text(text, separators)
-add_sentence(normalized_text)
-
-clear_text = normalized_text.replace(' iz ', ' is ')  # Replacing all “iz” with correct “is”, but only when it is a mistake
-
-final_text = re.sub(r'(\w)(“)', r'\1 \2', clear_text)  # Fix missing space before "iz"
-
-spaces_number = len(re.findall(r'\s', text))  # Finding the number of spaces
-
 if __name__ == "__main__":
+    separators = r'[.|:]\s*'
+    text = '''homEwork:
+        tHis iz your homeWork, copy these Text to variable.
+    
+        You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+    
+        it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+    
+        last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
+
+    normalized_text = normalize_text(text, separators)
+    create_and_add_sentence(normalized_text)
+    clear_text = normalized_text.replace(' iz ', ' is ')  # Replacing all “iz” with correct “is”, but only when it is a mistake
+    final_text = re.sub(r'(\w)(“)', r'\1 \2', clear_text)  # Fix missing space before "iz"
+    spaces_number = len(re.findall(r'\s', text))  # Finding the number of spaces
     print(final_text)
     print('Number of spaces =', spaces_number)
